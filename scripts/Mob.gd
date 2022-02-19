0409fc80e290816dd6f6bf95c012860b4dd52922
Vitor Hugo Magalhães Ramos <contato.vhmagalhaes@gmail.com>
2022-02-18 21:05:50 -0300

    Fixed many problems regarding Mob's collisions

--- scripts/Mob.gd
+++ scripts/Mob.gd
@@ -1,15 +1,22 @@
 extends Entity
 
+onready var animated_sprite = $AnimatedSprite
+onready var ledge_collision = $Raycasts/LedgeCollision
+onready var jump_collision = $Raycasts/JumpCollision
+onready var wall_collision = $Raycasts/WallCollision
+onready var top_collision = $EventCollisions/TopCollision
 
 func _ready():
 	set_physics_process(true)
-	$Skin/AnimatedSprite.play("walk")
+	animated_sprite.play("walk")
 
 func change_direction():
 	direction = direction * -1
-	$Raycasts/Ledge.position.x *= -1
-	$Raycasts/WallCollision.position.x *= -1
-	$Raycasts/WallCollision2.position.x *= -1
+	ledge_collision.position.x *= -1
+	wall_collision.position.x *= -1
+	wall_collision.cast_to *= Vector2(-1,0)
+	jump_collision.position.x *= -1
+	jump_collision.cast_to *= Vector2(-1,0)
 
 func process_horizontal_movement(delta: float):
 	.process_horizontal_movement(delta)
@@ -23,11 +30,11 @@ func process_horizontal_movement(delta: float):
 func _physics_process(delta: float):
 	physics_process(delta)
 
-func _on_Top_body_entered(body: Node):
-	if body.global_position.y > $EventCollisions/Top.global_position.y:
+func _on_TopCollision_body_entered(body: Node):
+	if body.global_position.y > top_collision.global_position.y - 5:
 		return
 	queue_free()
 
-func _on_Sides_body_entered(body):
+func _on_SideCollision_body_entered(body):
 	if body.is_in_group("Player") and body.invulnerable == false:
 		body.take_damage()
